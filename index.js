9005b7615700d80f310a9914339d8fc1e325c7f4
njleonzhang <scream3616@sina.com>
2018-06-28 11:37:38 +0800

    support webpack 4

--- index.js
+++ index.js
@@ -6,18 +6,40 @@ function HtmlWebpackInlinePlugin(options) {
 }
 
 HtmlWebpackInlinePlugin.prototype.apply = function(compiler) {
-  var self = this
-  compiler.plugin('compilation', function(compilation, options) {
-    compilation.plugin('html-webpack-plugin-before-html-processing', function(htmlPluginData, callback) {
-      var html = htmlPluginData.html
-      inline(html, self.options, function(err, html) {
-        if (!err) {
-          htmlPluginData.html = html
+  if (compiler.hooks) {
+    compiler.hooks.compilation.tap('HtmlWebpackInlinePlugin', compilation => {
+      if (!compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing) {
+        throw new Error('The expected HtmlWebpackPlugin hook was not found! Ensure HtmlWebpackPlugin is installed and was initialized before this plugin.');
+      }
+
+      compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync(
+        'HtmlWebpackInlinePlugin',
+        (htmlPluginData, callback) => {
+          var html = htmlPluginData.html
+          console.log(html)
+          inline(html, this.options, function(err, html) {
+            if (!err) {
+              htmlPluginData.html = html
+              console.log(htmlPluginData.html)
+            }
+            callback(null, htmlPluginData)
+          })
         }
-        callback(null, htmlPluginData)
+      )
+    })
+  } else {
+    compiler.plugin('compilation', function(compilation, options) {
+      compilation.plugin('html-webpack-plugin-before-html-processing', (htmlPluginData, callback) => {
+        var html = htmlPluginData.html
+        inline(html, self.options, function(err, html) {
+          if (!err) {
+            htmlPluginData.html = html
+          }
+          callback(null, htmlPluginData)
+        })
       })
-    });
-  });
+    })
+  }
 };
 
 module.exports = HtmlWebpackInlinePlugin
