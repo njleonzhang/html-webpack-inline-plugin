f4fd41879a80bb916de5b2cdd82a2224947a71e9
Kaushik Ganguly <kganguly@gmail.com>
2019-11-11 23:59:43 -0500

    * Enable usage with `html-webpack-plugin` v4 by   using its new `afterTemplateExecution` hook * Use `class` to simplify * Upgrade to v6 of `inline-source` to use Promises and `async/await` * Enforce minimum Node version for `aync/await`

--- index.js
+++ index.js
@@ -1,46 +1,55 @@
-var inline = require('inline-source')
+const { inlineSource } = require('inline-source');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
-function HtmlWebpackInlinePlugin(options) {
-  // Setup the plugin instance with options...
-  this.options = options
-}
-
-HtmlWebpackInlinePlugin.prototype.apply = function(compiler) {
-  let self = this
-  if (compiler.hooks) {
-    compiler.hooks.compilation.tap('HtmlWebpackInlinePlugin', compilation => {
-      if (!compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing) {
-        throw new Error('The expected HtmlWebpackPlugin hook was not found! Ensure HtmlWebpackPlugin is installed and was initialized before this plugin.');
-      }
-
-      compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync(
-        'HtmlWebpackInlinePlugin',
-        (htmlPluginData, callback) => {
-          var html = htmlPluginData.html
-          inline(html, self.options, function(err, html) {
-            if (err) {
-              return callback(err)
-            }
-            htmlPluginData.html = html
-            callback(null, htmlPluginData)
-          })
-        }
-      )
-    })
-  } else {
-    compiler.plugin('compilation', (compilation, options) => {
-      compilation.plugin('html-webpack-plugin-before-html-processing', (htmlPluginData, callback) => {
-        var html = htmlPluginData.html
-        inline(html, self.options, function(err, html) {
-          if (err) {
-            return callback(err)
-          }
-          htmlPluginData.html = html
-          callback(null, htmlPluginData)
-        })
-      })
-    })
+class HtmlWebpackInlinePlugin {
+  constructor(options) {
+    // Setup the plugin instance with options...
+    this.options = options
   }
-};
+
+  apply(compiler) {
+    let self = this
+
+    if (compiler.hooks) {
+      // webpack 4 support
+      compiler.hooks.compilation.tap('HtmlWebpackInlinePlugin', compilation => {
+        const afterTemplateExecutionHook = htmlWebpackPlugin.getHooks(
+          compilation
+        ).afterTemplateExecution
+
+        if (!afterTemplateExecutionHook) {
+          throw new Error(
+            'The expected HtmlWebpackPlugin hook was not found! Ensure HtmlWebpackPlugin is installed and was initialized before this plugin.'
+          );
+        }
+
+        afterTemplateExecutionHook.tapPromise(
+          'HtmlWebpackInlinePlugin',
+          async htmlPluginData => {
+            htmlPluginData.html = await inlineSource(
+              htmlPluginData.html,
+              self.options
+            )
+
+            return Promise.resolve()
+          }
+        )
+      })
+    } else {
+      compiler.plugin('compilation', compilation => {
+        compilation.plugin(
+          'html-webpack-plugin-before-html-processing',
+          async (htmlPluginData, callback) => {
+            htmlPluginData.html = await inlineSource(
+              htmlPluginData.html,
+              self.options
+            )
+            callback(null, htmlPluginData)
+          }
+        )
+      })
+    }
+  }
+}
 
 module.exports = HtmlWebpackInlinePlugin
